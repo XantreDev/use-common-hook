611f3e8b7c63fce8de3f6a4684424688b828bfd5
XantreGodlike <valerik007.vs@gmail.com>
2022-10-27 12:34:01 +0300

    import commas added

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ export { useIsFirstCall } from "./hooks/useIsFirstCall";
 export { useMemoCompare } from "./hooks/useMemoCompare";
 export {
   useOnChange as useOnChangeCompare,
-  useOnChangeWithoutInit as useOnChangeWithoutInitCompare
+  useOnChangeWithoutInit as useOnChangeWithoutInitCompare,
 } from "./hooks/useOnChange";
 export {
   useOnChange as useOnChangeDeep,
-  useOnChangeWithoutInit as useOnChangeWithoutInitDeep
+  useOnChangeWithoutInit as useOnChangeWithoutInitDeep,
 } from "./hooks/useOnChange/deep";
 export {
   useOnChange,
-  useOnChangeWithoutInit
+  useOnChangeWithoutInit,
 } from "./hooks/useOnChange/shallow";
 export { usePreviousValue } from "./hooks/usePreviousValue";
 export { useRenderEffect, useRenderEffectDeep } from "./hooks/useRenderEffect";
@@ -20,7 +20,6 @@ export { useStabilizedValue } from "./hooks/useStabilizedValue";
 export { useStableCallback } from "./hooks/useStableCallback";
 export {
   useStateWithGetter,
-  UseStateWithGetterResult
+  UseStateWithGetterResult,
 } from "./hooks/useStateWithGetter";
 export { useToggle } from "./hooks/useToggle";
-
